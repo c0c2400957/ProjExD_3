9ce66ec442cdcb0a5f47786644674fd6348b945c
C0C24009 <c0c2400957@edu.teu.ac.jp>
2025-10-07 15:36:39 +0900

    追加機能1: スコア表示機能を実装

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -108,6 +108,25 @@ class Bomb:
         self.rct.move_ip(self.vx, self.vy)
         screen.blit(self.img, self.rct)
 
+class Score:
+    """
+    スコアを表示するクラス
+    """
+    def __init__(self):
+        self.font = pg.font.SysFont("hgp創英角ポップ体", 30)
+        self.color = (0, 0, 255)
+        self.score = 0
+        self.img = self.font.render(f"スコア: {self.score}", 0, self.color)
+        self.rct = self.img.get_rect()
+        self.rct.center = (100, HEIGHT - 50)
+
+    def score_up(self, add):
+        self.score += add
+
+    def update(self, screen: pg.Surface):
+        self.img = self.font.render(f"スコア: {self.score}", 0, self.color)
+        screen.blit(self.img, self.rct)
+
 
 def main():
     pg.display.set_caption("たたかえ！こうかとん")
@@ -116,6 +135,7 @@ def main():
     bird = Bird((300, 200))
     bombs = [Bomb((255, 0, 0), 10) for _ in range(NUM_OF_BOMBS)]
     beam = None
+    score = Score() # Scoreインスタンスを生成
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -145,6 +165,7 @@ def main():
                         beam = None
                         bombs[i] = None
                         bird.change_img(6, screen) # 喜びエフェクト
+                        score.score_up(1) #スコアを1点加算
 
         # Noneでない爆弾だけのリストに更新
         bombs = [bomb for bomb in bombs if bomb is not None]
@@ -160,6 +181,7 @@ def main():
             if not check_bound(beam.rct)[0]:
                 beam = None
 
+        score.update(screen) #スコアを描画
         pg.display.update()
         tmr += 1
         clock.tick(50)
